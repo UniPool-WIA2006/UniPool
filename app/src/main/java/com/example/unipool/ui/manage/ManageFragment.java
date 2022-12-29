c383f55fd30f8219899de960b7feaac69d82e460
Marvin Chin Yi Kai <u2000490@siswa.um.edu.my>
2022-12-30 02:16:14 +0800

    Add bottom navigation bar

--- app/src/main/java/com/example/unipool/ui/manage/ManageFragment.java
+++ app/src/main/java/com/example/unipool/ui/manage/ManageFragment.java
@@ -1,4 +1,4 @@
-package com.example.unipool.ui.notifications;
+package com.example.unipool.ui.manage;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.unipool.databinding.FragmentNotificationsBinding;
+import com.example.unipool.databinding.FragmentHomeBinding;
 
-public class NotificationsFragment extends Fragment {
+public class ManageFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        NotificationsViewModel notificationsViewModel =
-                new ViewModelProvider(this).get(NotificationsViewModel.class);
+        ManageViewModel homeViewModel =
+                new ViewModelProvider(this).get(ManageViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/example/unipool/ui/manage/ManageViewModel.java
+++ app/src/main/java/com/example/unipool/ui/manage/ManageViewModel.java
@@ -1,16 +1,16 @@
-package com.example.unipool.ui.dashboard;
+package com.example.unipool.ui.manage;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ManageViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ManageViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is manage fragment");
     }
 
     public LiveData<String> getText() {
