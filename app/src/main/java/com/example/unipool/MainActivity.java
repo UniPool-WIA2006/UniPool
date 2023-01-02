66d7020cb85c6492071b2b199d00c4cca12463d7
Muhamad Izzul Izzani <izzulizzani2002@gmail.com>
2023-01-02 19:18:33 +0800

    home

--- app/src/main/java/com/example/unipool/MainActivity.java
+++ app/src/main/java/com/example/unipool/MainActivity.java
@@ -1,33 +1,19 @@
 package com.example.unipool;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.example.unipool.ui.chat.ChatFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.appcompat.widget.Toolbar;
 
 import com.example.unipool.databinding.ActivityMainBinding;
-
-import io.getstream.chat.android.client.ChatClient;
-import io.getstream.chat.android.client.logger.ChatLogLevel;
-import io.getstream.chat.android.client.models.User;
-import io.getstream.chat.android.offline.plugin.configuration.Config;
-import io.getstream.chat.android.offline.model.message.attachments.UploadAttachmentsNetworkType;
-import io.getstream.chat.android.offline.plugin.factory.StreamOfflinePluginFactory;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +22,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        //Action Bar
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-        // Display application icon in the toolbar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        //Bottom Navigation
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -51,42 +29,8 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_manage, R.id.navigation_chat, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        // Setting up Chat with offline function
-        Config config = new Config(true, true, true, UploadAttachmentsNetworkType.NOT_ROAMING);
-        StreamOfflinePluginFactory offlinePluginFactory = new StreamOfflinePluginFactory(config, this);
-        ChatClient client = new ChatClient.Builder(getString(R.string.api_key), this).logLevel(ChatLogLevel.ALL).withPlugin(offlinePluginFactory).build();
-
-        Intent intent = getIntent();
-        username = intent.getStringExtra("username");
-
-        Toast.makeText(MainActivity.this, "Welcome " + username, Toast.LENGTH_SHORT).show();
-
-        // Authenticate and connect the user
-//        User user = new User();
-//        user.setId(username);
-//        user.setName(username);
-//        user.setImage("https://bit.ly/2TIt8NR");
-//        String token = client.devToken(user.getId());
-//
-//        String token = client.devToken(user.getId());
-//
-//        client.connectUser(user, token).enqueue(result -> {
-//            if(result.isSuccess()) {
-//                Log.d( "ChatFragment", "Success Connecting");
-//            } else {
-//                Log.d( "ChatFragment", result.error().getMessage().toString());
-//            }
-//        });
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
--- app/src/main/java/com/example/unipool/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/unipool/ui/profile/ProfileFragment.java
@@ -1,30 +1,31 @@
-package com.example.unipool.ui.home;
+package com.example.unipool.ui.profile;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.unipool.databinding.FragmentHomeBinding;
+import com.example.unipool.databinding.FragmentProfileBinding;
 
-public class HomeFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
-    private FragmentHomeBinding binding;
+    private FragmentProfileBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        ProfileViewModel homeViewModel =
+                new ViewModelProvider(this).get(ProfileViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textHome;
-//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textDashboard;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
