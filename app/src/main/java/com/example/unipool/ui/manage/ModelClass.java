848e6f9b45153faca1905feb9ff2eb884a783ad1
Muhamad Izzul Izzani <izzulizzani2002@gmail.com>
2023-01-29 08:08:08 +0800

    -29/1 8.07 -done copy paste dari branch izzul ke main

---
+++ app/src/main/java/com/example/unipool/ui/manage/ModelClass.java
@@ -0,0 +1,58 @@
+package com.example.unipool.ui.manage;
+
+public class ModelClass {
+    private int rvPicture, rvGenderIcon;
+    private String rvName;
+    private String rvNumber;
+    private String rvFees;
+    private String rvLocation;
+    private String rvDestination;
+    private int rvRating;
+    private String rvExtraNotes;
+
+    public ModelClass(int rvPicture, int rvGenderIcon, String rvName, String rvNumber, String rvFees, String rvLocation, String rvDestination) {
+        this.rvPicture = rvPicture;
+        this.rvGenderIcon = rvGenderIcon;
+        this.rvName = rvName;
+        this.rvNumber = rvNumber;
+        this.rvFees = rvFees;
+        this.rvLocation = rvLocation;
+        this.rvDestination = rvDestination;
+    }
+
+    public int getRvPicture() {
+        return rvPicture;
+    }
+
+    public int getRvGenderIcon() {
+        return rvGenderIcon;
+    }
+
+    public String getRvName() {
+        return rvName;
+    }
+
+    public String getRvNumber() {
+        return rvNumber;
+    }
+
+    public String getRvFees() {
+        return rvFees;
+    }
+
+    public String getRvLocation() {
+        return rvLocation;
+    }
+
+    public String getRvDestination() {
+        return rvDestination;
+    }
+
+    public int getRvRating() {
+        return rvRating;
+    }
+
+    public String getRvExtraNotes() {
+        return rvExtraNotes;
+    }
+}
